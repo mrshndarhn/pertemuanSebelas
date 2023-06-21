4d01c9f3a2c59bcc8d31959ffbbe764f6be5c3d9
Marshanda Raihan Widyaswari <20102020@ittelkom-pwt.ac.id>
2023-06-21 12:16:42 +0700

    Initial commit

---
+++ app/src/main/java/com/marshanda/pertemuansebelas/model/Mahasiswa.kt
@@ -0,0 +1,4 @@
+package com.marshanda.pertemuansebelas.model
+
+data class Mahasiswa(val nim : String = " ", val nama : String = " ", val telepon : String = " ")
+
---
+++ app/src/main/java/com/marshanda/pertemuansebelas/view/HomeFragment.kt
@@ -0,0 +1,56 @@
+package com.marshanda.pertemuansebelas.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.database.*
+import com.marshanda.pertemuansebelas.R
+import com.marshanda.pertemuansebelas.adapter.HomeAdapter
+import com.marshanda.pertemuansebelas.databinding.FragmentHomeBinding
+import com.marshanda.pertemuansebelas.model.Mahasiswa
+
+class HomeFragment : Fragment() {
+    lateinit var binding : FragmentHomeBinding
+    lateinit var adapter : HomeAdapter
+    lateinit var databaseRef : DatabaseReference
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = HomeAdapter(ArrayList())
+        binding.rvUser.adapter = adapter
+        binding.rvUser.layoutManager = LinearLayoutManager(requireContext())
+
+        databaseRef = FirebaseDatabase.getInstance().getReference("mahasiswa")
+        databaseRef.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val mahasiswaList = ArrayList<Mahasiswa>()
+                for (snapshot in dataSnapshot.children){
+                    val mahasiswa = snapshot.getValue(Mahasiswa::class.java)
+                    mahasiswa?.let{
+                        mahasiswaList.add(it)
+                    }
+                    adapter.setData(mahasiswaList)
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                // Handler Error
+            }
+
+        })
+    }
+
+}
